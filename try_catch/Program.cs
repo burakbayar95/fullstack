38bfad0ef31c0da16a488f5e70a1ece7715f70fa
Burak BAYAR <95burakbayar@gmail.com>
2021-06-01 01:08:18 +0300

    v4

---
+++ try_catch/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace try_catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir sayi giriniz: \n");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş olduğunuz sayi: " + sayi);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata: " + ex.Message.ToString());
+
+            }
+            finally
+            {
+                // hata alsın ya da almasın burası gösterilir.
+                Console.WriteLine("İşlem Tamamlandı..");
+
+            }
+
+            try
+            {
+                //parse=> string ifadeleri int e çevirir
+                //int a=int.Parse(null);
+                int b=int.Parse("test");
+
+            }
+            catch (ArgumentNullException ex)
+            {
+                
+                 Console.WriteLine("Boş değer girdiniz\n");
+                   Console.WriteLine(ex);
+            }
+
+            catch(FormatException ex2)
+            {
+                Console.WriteLine("Veri tipi uygun değil\n");
+                   Console.WriteLine(ex2);
+                
+
+            }
+
+
+
+        }
+    }
+}
