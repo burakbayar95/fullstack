38e56dfd50f9c25d4e9eece20400c44bf2a08b84
Burak BAYAR <95burakbayar@gmail.com>
2021-05-31 20:34:50 +0300

    Update_V2

---
+++ Operatorler/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Operatorler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int x= 3;
+           int y=5;
+           x++;
+           y++;
+           y=y+2;
+           y+=2;
+           Console.Write(y+"\n");
+
+           int a=5;
+           int b=5;
+           int c=5;
+           if(a==b && c==5)
+           {
+               Console.Write("Selam \n");
+           }
+
+             if(a==b || c==4)
+           {
+               Console.Write("Selam Kanka");
+           }
+
+           // % mod alma
+
+
+        }
+    }
+}
