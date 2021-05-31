fa092a5cef310aa041398be88156509c5f214276
Burak BAYAR <95burakbayar@gmail.com>
2021-06-01 01:50:25 +0300

    v6

---
+++ switch_case/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int month = DateTime.Now.Month;
+            switch (month)
+            {
+                case 1:
+
+                    Console.WriteLine("Ocak");
+                    break;
+
+                case 6:
+
+                    Console.WriteLine("Haziran");
+                    break;
+
+
+                default:
+                    Console.WriteLine("Yanlış Veri Girişi..");
+                    break;
+            }
+
+            switch (month)
+            {
+
+                case 1:
+                case 2:
+                case 3:
+                 Console.WriteLine("Kış Ayı");
+                 break;
+
+                
+                default:
+                break;
+            }
+        }
+    }
+}
