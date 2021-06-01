f95b069ffa52937e23702fd5acb4fc524343c466
Burak BAYAR <95burakbayar@gmail.com>
2021-06-01 17:53:14 +0300

    v9

---
+++ dizi/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace dizi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] renkler = new string[5];
+            string[] hayvanlar = { "kedi", "kuş" };
+            int[] dizi;
+            dizi = new int[5];
+
+            renkler[0] = "mavi";
+
+            //-----
+            Console.WriteLine("Dizinin eleman sayisini giriniz: ");
+            int diziuzunlugu = int.Parse(Console.ReadLine());
+            int[] sayi = new int[diziuzunlugu];
+            for (int i = 0; i < diziuzunlugu; i++)
+            {
+                Console.Write("Lütfen {0}.sayiyi giriniz", i + 1);
+                sayi[i] = int.Parse(Console.ReadLine());
+            }
+            int toplam = 0;
+            foreach (var item in sayi)
+            {
+                toplam += item;
+            }
+            Console.WriteLine("Ortalama: "+ toplam/diziuzunlugu);
+
+
+        }
+    }
+}
