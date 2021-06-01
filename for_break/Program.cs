5b44f9c8a6e9f559a7270d7d58c0b8d28f431199
Burak BAYAR <95burakbayar@gmail.com>
2021-06-01 17:00:24 +0300

    v7

---
+++ for_break/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace for_break
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayi Giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            int teksayilartoplami=0;
+           int ciftsayilartoplami=0;
+
+
+/*
+
+            for (int i = 1; i <= sayi; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+
+                }
+
+            }
+*/
+            for (int i = 0; i <= 1000; i++)
+            {
+                 if (i % 2 == 1)
+                {
+                teksayilartoplami+=i; //teksayilartoplami=i+teksayilartoplami;
+
+                }
+
+                if (i%2==0)
+                {
+                    ciftsayilartoplami+=i;//ciftsayilartoplami=i+ciftsayilartoplami;
+
+                }
+
+                
+            }
+
+            int toplam =teksayilartoplami+ciftsayilartoplami;
+            Console.WriteLine("Tek sayılar toplamı= "+teksayilartoplami);
+             Console.WriteLine("Çift sayılar toplamı= "+ciftsayilartoplami);
+            Console.WriteLine("Toplam= "+toplam);
+            
+            
+
+
+        }
+    }
+}
