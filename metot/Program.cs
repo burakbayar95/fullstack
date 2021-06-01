6c52b57d7d159454ab2df06fcff2935b23e762c4
Burak BAYAR <95burakbayar@gmail.com>
2021-06-01 19:59:03 +0300

    v11

---
+++ metot/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace metot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a=5;
+            int b=10;
+           int sonuc=(topla(a,b));
+
+            Metotlar ornek= new Metotlar();
+            ornek.ekranayazdir(sonuc.ToString());
+
+
+
+
+        }
+        static int topla(int deger1,int deger2)
+        {
+            return deger1+deger2;
+        }
+    }
+
+    class Metotlar
+    {
+        public void ekranayazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+    }
+}
