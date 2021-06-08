bc0a55f139e204e2cb2f18f2f95d46c8ca2a59a1
Burak <95burakbayar@gmail.com>
2021-06-08 22:08:49 +0300

    v18

---
+++ Nesne/Nesne/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Nesne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Calisan calisan1 = new Calisan();
+            calisan1.ad = "Burak";
+
+
+            calisan1.CalisanBilgileri();
+
+
+       
+
+        }
+    }
+
+    class Calisan
+    {
+        public string ad;
+        public int no;
+
+        public void CalisanBilgileri()
+        {
+            Console.WriteLine("Çalışan Adi {0}", ad);
+        }
+
+    }
+}
