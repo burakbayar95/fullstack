b7bf0a6571f54b92cec9a5a6531e67e7ff504475
Burak <95burakbayar@gmail.com>
2021-06-09 02:18:26 +0300

    v22

---
+++ Struct_/Struct_/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Struct_
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Dikdortgen dk = new Dikdortgen();
+            dk.kisakenar = 3;
+            dk.uzunkenar = 4;
+            Console.WriteLine("Alan: {0}", dk.AlanHesapla());
+
+            DikDortgenStruct dkst; // new ile oluşturmadan da kullanılabilir.
+            dkst.kisakenar = 3;
+            dkst.uzunkenar = 4;
+            Console.WriteLine("Alan: {0}", dkst.AlanHesapla());
+        }
+    }
+
+    internal class Dikdortgen
+    {
+        public int kisakenar;
+        public int uzunkenar;
+
+        public long AlanHesapla()
+        {
+            return this.kisakenar * this.uzunkenar;
+        }
+    }
+
+    internal struct DikDortgenStruct
+    {
+        public int kisakenar;
+        public int uzunkenar;
+
+        public long AlanHesapla()
+        {
+            return this.kisakenar * this.uzunkenar;
+        }
+    }
+}
