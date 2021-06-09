b5da83d808e277c6125d98cccce3bd68bf0ac3a3
Burak <95burakbayar@gmail.com>
2021-06-09 03:47:23 +0300

    v24

---
+++ Interfaces_/Interfaces_/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Interfaces_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Smslogger sms = new Smslogger();
+            sms.WriteLog();
+        }
+    }
+
+    public interface Ilogger
+    {
+        void WriteLog();
+
+    }
+
+    public interface Databaselogger : Ilogger
+    {
+        public void WriteLog()
+        {
+            Console.WriteLine("Database'e  yazar");
+
+        }
+
+    }
+
+    public class Smslogger :Ilogger
+    {
+        public void WriteLog()
+        {
+            Console.WriteLine("Dosyaya log yazar");
+
+        }
+    }
+}
