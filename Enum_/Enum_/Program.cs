ca6c6e013376e0141f439bf5a48135dbe41aa473
Burak <95burakbayar@gmail.com>
2021-06-09 03:08:31 +0300

    v23

---
+++ Enum_/Enum_/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace Enum_
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Gunler.Carsambe);
+            Console.WriteLine((int)Gunler.Pazaretesi);
+           
+
+            int sicaklik = 32;
+            if (sicaklik<=(int)HavaDurumu.Normal)
+            {
+                Console.WriteLine("Dışarı çıkmak için sıkıntı");
+            }
+           
+        }
+    }
+
+    internal enum Gunler
+    {
+        Pazaretesi = 1,
+        Salı,
+        Carsambe,
+        Persembe
+    }
+
+     enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal = 20,
+        Sıcak = 25
+    }
+}
